683bb6d56059e70bf8588cd001404c7c7e4282e8
SrJota <102335464+srjotaoff@users.noreply.github.com>
2024-07-22 12:33:21 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 let biblioteca = [];
 
-const biblioteca_from_localstorage = localStorage.getItem("biblioteca")
-if (biblioteca_from_localstorage != null){
-  biblioteca = JSON.parse(biblioteca_from_localstorage)
-} else {
-  biblioteca = [{palavra: "ornitorrinco" , dica: "animal exotico"},
+let biblioteca_padrao = [{palavra: "ornitorrinco" , dica: "animal exotico"},
   {palavra:"bergamota" , dica:"fruta nordestina"} ,
   {palavra:"palmeira" , dica:"tipo de arvore"} ,
   {palavra:"bentivi" , dica:"um passaro"} ,
@@ -17,7 +13,34 @@ if (biblioteca_from_localstorage != null){
   {palavra:"ichia" , dica:"uma fruta"} ,
   {palavra:"mangostim" , dica:"uma fruta baiana"} ,
   {palavra:"dromedario" , dica:"um animal"} ,
-  {palavra:"tecnologia" , dica:"sem ela a humanidade não seria nada"}];
+  {palavra:"tecnologia" , dica:"sem ela a humanidade não seria nada"} ,
+  {palavra: "abacaxi", dica: "fruta tropical"} ,
+  {palavra: "girassol", dica: "flor que segue o sol"} ,
+  {palavra: "gato", dica: "animal doméstico"} ,
+  {palavra: "cachorro", dica: "melhor amigo do homem"} ,
+  {palavra: "computador", dica: "máquina eletrônica"} ,
+  {palavra: "mesa", dica: "móvel para refeições"} ,
+  {palavra: "aviao", dica: "meio de transporte aéreo"} ,
+  {palavra: "banana", dica: "fruta amarela"} ,
+  {palavra: "piano", dica: "instrumento musical de cordas"} ,
+  {palavra: "cama", dica: "móvel para dormir"} ,
+  {palavra: "sapato", dica: "calçado"} ,
+  {palavra: "arvores", dica: "plantas altas"} ,
+  {palavra: "livro", dica: "contém histórias"} ,
+  {palavra: "ventilador", dica: "aparelho que gera vento"} ,
+  {palavra: "cozinha", dica: "local onde se prepara comida"} ,
+  {palavra: "praia", dica: "lugar de areia e mar"} ,
+  {palavra: "teclado", dica: "parte do computador para digitar"} ,
+  {palavra: "caneta", dica: "escreve tinta"} ,
+  {palavra: "bola", dica: "usada em diversos esportes"} ,
+  {palavra: "fogao", dica: "utilizado para cozinhar"}];
+
+
+const biblioteca_from_localstorage = localStorage.getItem("biblioteca")
+if (biblioteca_from_localstorage != null){
+  biblioteca = JSON.parse(biblioteca_from_localstorage)
+} else {
+  biblioteca = biblioteca_padrao;
 }
 
 const numero_max = (biblioteca.length - 1);
@@ -103,22 +126,9 @@ function jogar (key) {
 
 
 
-function adicionar_palavra (){
+function adicionar_palavra() {
   if (biblioteca_from_localstorage == null) {
-    biblioteca = [{palavra: "ornitorrinco" , dica: "animal exotico"},
-    {palavra:"bergamota" , dica:"fruta nordestina"} ,
-    {palavra:"palmeira" , dica:"tipo de arvore"} ,
-    {palavra:"bentivi" , dica:"um passaro"} ,
-    {palavra:"abelha" , dica:"tem uma bundona"} ,              
-    {palavra:"leao" , dica:"rei da selva"} ,
-    {palavra:"macadamia" , dica:"é uma noz"} ,
-    {palavra:"hamburguer" , dica:"melhor fast-food"} ,
-    {palavra:"lavanda" , dica:"aroma agradavel"} ,
-    {palavra:"enrolados" , dica:"filme da disney"} ,
-    {palavra:"ichia" , dica:"uma fruta"} ,
-    {palavra:"mangostim" , dica:"uma fruta baiana"} ,
-    {palavra:"dromedario" , dica:"um animal"} ,
-    {palavra:"tecnologia" , dica:"sem ela a humanidade não seria nada"}];
+    biblioteca = biblioteca_padrao;
   }
 
   biblioteca.push({palavra: document.getElementById("palavra_nova").value, dica: document.getElementById("dica_palavra_nova").value});
